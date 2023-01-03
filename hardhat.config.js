d42f1bf2d5351ab8270e4f8fad58829b17d08395
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-02 20:34:29 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,25 +12,21 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 2330
     },
     mumbai: {
-      url: "https://rpc0.altcoinchain.org/rpc",
-      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+      url: `https://rpc0.altcoinchain.org/rpc`,
+      accounts: [process.env.REACT_PRIVATE_KEY,],
       gas: 30000000,
       gasPrice: 7000000000
     },
- //   matic: {
-//      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
-      //accounts: [process.env.privateKey]
-//    },
-   // goerli: {
-  //   url: "https://rpc0.altcoinchain.org/rpc",
-  //    accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
- //   }    
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY, process.env.ACCOUNT2_PRIVATE_KEY]
+    }
   },
   solidity: {
     version: "0.8.4",
